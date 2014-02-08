2819b25b449c4231e37c14e5fdc6d6009521726b
Jeremy Meyer <jjman505@gmail.com>
2014-02-08 16:14:16 -0500

    made some changes

--- router.js
+++ router.js
@@ -1,3 +1,5 @@
+
+
 Router.configure({
   layoutTemplate: 'layout'
 });
@@ -5,50 +7,98 @@ Router.configure({
 Router.map(function () {
   this.route('home', {
     path: '/',
-    template: 'main'
+    template: 'main',
+    after: function() {
+      $('.content-inner').scrollTop(0);
+      $(window).scrollTop(0);
+    }
   });
   this.route('topics-index', {
     path: '/topics',
-    template: 'topics-index'
+    template: 'topics-index',
+    after: function() {
+      $('.content-inner').scrollTop(0);
+      $(window).scrollTop(0);
+    }
   });
   this.route('why-meteor', {
     path: '/topics/why-meteor',
-    template: 'why-meteor'
+    template: 'why-meteor',
+    after: function() {
+      $('.content-inner').scrollTop(0);
+      $(window).scrollTop(0);
+    }
   });
   this.route('getting-started', {
     path: '/topics/getting-started',
-    template: 'getting-started'
+    template: 'getting-started',
+    after: function() {
+      $('.content-inner').scrollTop(0);
+      $(window).scrollTop(0);
+    }
   });
   this.route('first-template', {
     path: '/topics/first-template',
-    template: 'first-template'
+    template: 'first-template',
+    after: function() {
+      $('.content-inner').scrollTop(0);
+      $(window).scrollTop(0);
+    }
   });
   this.route('videos', {
     path: '/videos',
-    template: 'videos'
+    template: 'videos',
+    after: function() {
+      $('.content-inner').scrollTop(0);
+      $(window).scrollTop(0);
+    }
   });
   this.route('challenges', {
     path: '/challenges',
-    template: 'challenges'
+    template: 'challenges',
+    after: function() {
+      $('.content-inner').scrollTop(0);
+      $(window).scrollTop(0);
+    }
   });
   this.route('signup', {
     path: '/signup',
-    template: 'signup'
+    template: 'signup',
+    after: function() {
+      $('.content-inner').scrollTop(0);
+      $(window).scrollTop(0);
+    }
   });
   this.route('about', {
     path: '/about',
-    template: 'about'
+    template: 'about',
+    after: function() {
+      $('.content-inner').scrollTop(0);
+      $(window).scrollTop(0);
+    }
   });
   this.route('jobs', {
     path: '/jobs',
-    template: 'jobs'
+    template: 'jobs',
+    after: function() {
+      $('.content-inner').scrollTop(0);
+      $(window).scrollTop(0);
+    }
   });
   this.route('blog', {
     path: '/blog',
-    template: 'blog-main'
+    template: 'blog-main',
+    after: function() {
+      $('.content-inner').scrollTop(0);
+      $(window).scrollTop(0);
+    }
   });
   this.route('contact', {
     path: '/contact',
-    template: 'contact'
+    template: 'contact',
+    after: function() {
+      $('.content-inner').scrollTop(0);
+      $(window).scrollTop(0);
+    }
   });
 });
