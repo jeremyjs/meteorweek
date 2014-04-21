e995f50f9bfcd98e658ec75c44b24de735bfa525
Jeremy Meyer <jjman505@gmail.com>
2014-04-21 17:28:43 -0400

    Addition to "intro-collections"

--- collections/lessons.js
+++ collections/lessons.js
@@ -1,47 +1,12 @@
-Lessons = new Meteor.Collection("lessons");
-
-titles = [
-	"whyMeteor",
-	"gettingStarted",
-	"myFirstTemplate",
-	"routingBasics"
-];
-
-insertTitles(titles);
-
-function insertTitles (titles) {
-  if(titles.length == 0) {
-    return;
-  }
-  else if(titles.length == 1) {
-    Lessons.insert({
-      title: titles[0],
-      prev: "",
-      next: ""
-    });
-  }
-  else if(titles.length == 2) {
-    Lessons.insert({
-      title: titles[0],
-      prev: "",
-      next: ""
-    });
-  }
-  Lessons.insert({
-    title: titles[0],
-    prev: "",
-    next: titles[1]
-  });
-  for (var i = 1; i < titles.length-1; i++) {
-    Lessons.insert({
-      title: titles[i],
-      prev: titles[i-1],
-      next: titles[i+1]
-    });
-  }
-  Lessons.insert({
-  	title: titles[titles.length-1],
-    prev: titles[titles.length-2],
-    next: ""
-  });
-}
+var lessons = [
+  ["", ""],
+  // ["whyMeteor", "Why Meteor"],
+  ["gettingStarted", "Getting Started"],
+  ["firstTemplate", "Your First Template"],
+  ["introCollections", "Intro to Collections"],
+  ["programmerToolbox", "The Programmer's Toolbox"],
+  ["routingBasics", "Routing Basics"],
+  ["publishSubscribe", "Publish & Subscribe"],
+  ["nextSteps", "Next Steps"],
+  ["", ""]
+]
