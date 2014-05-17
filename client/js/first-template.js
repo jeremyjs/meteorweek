b2b06118795b53b06daa678620becadccdb74093
Jeremy Meyer <jjman505@gmail.com>
2014-05-16 23:06:57 -0400

    Changes to "First Template"

--- client/js/first-template.js
+++ client/js/first-template.js
@@ -1,27 +1,29 @@
 if(Meteor.isClient) {
   Template.firstTemplate.onamae = function() {
-    return "Donald Knuth";
-  }
+    return "Jonathan Ive";
+  };
   Template.firstTemplate.names = function() {
-    return ["Donald Knuth", "Donald Trump", "Donald Duck"];
-  }
-  Template.firstTemplate.isTrump = function() {
-    return this.name === "Donald Trump";
-  }
+    return ["Jonathan Ive", "Bill Gates", "Larry Page"];
+  };
+  Template.firstTemplate.isGates = function() {
+    return this.name === "Bill Gates";
+  };
   Template.firstTemplate.people = function() {
-    return [{
-      "name" : "Donald Knuth",
-      "color" : "Blue"
-    },
-    {
-      "name" : "Donald Trump",
-      "color" : "Green"
-    },
-    {
-      "name" : "Donald Duck",
-      "color" : "Red"
-    }];
-  }
+    return [
+      {
+        "name" : "Jonathan Ive",
+        "color" : "red"
+      },
+      {
+        "name" : "Bill Gates",
+        "color" : "blue"
+      },
+      {
+        "name" : "Larry Page",
+        "color" : "green"
+      }
+    ];
+  };
   Template.firstTemplate.events({
     'click input' : function(){
       alert("You clicked the button!");
